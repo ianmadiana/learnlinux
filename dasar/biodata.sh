5d2d7eaa22ce74d41c563385fd6d5fc3e745f36b
ianmadiana <iyanmadiana28@gmail.com>
2025-12-24 11:47:54 +0000

    learn UID

--- dasar/biodata.sh
+++ dasar/biodata.sh
@@ -2,6 +2,16 @@
 
 # Script biodata input dari user
 
+# display computer username
+USER_NAME=$(id -un)
+echo "Your UID is ${USER_NAME}" # or you can use whoami
+# older syntax use backticks
+USER_NAME1=`whoami`
+echo "Username is ${USER_NAME1}"
+
+
+# display UID
+
 echo -n "Input your name: "
 # minta input dan masukkan ke variable NAME
 read NAME
