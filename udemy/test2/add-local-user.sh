d82ceb3e253169e74c02014fb258dee943c7bceb
ianmadiana <iyanmadiana28@gmail.com>
2026-01-08 01:45:13 +0000

    fix and change name script

--- udemy/test2/add-local-user.sh
+++ udemy/test2/add-local-user.sh
@@ -9,32 +9,30 @@ if [ ${UID} -eq 0 ]; then
     read -p "Input real name : " COMMENT
     read -s -p "Input password : " PASSWORD
 
-    useradd -c "${COMMENT}" -m ${USERNAME}
+    useradd -c "${COMMENT}" -m -s /bin/bash ${USERNAME} || {
+	    echo "Failed to create user"
+    exit 1
+}
 
-    if [[ -d /home/$USERNAME ]]
-    then
-	    echo "Reuse home directory for ${USERNAME}"
-	    chown -R "$USERNAME:$USERNAME" "/home/$USERNAME"
-    fi
+    # if [[ -d /home/$USERNAME ]]
+    # then
+	#     echo "Reuse home directory for ${USERNAME}"
+	#     chown -R "$USERNAME:$USERNAME" "/home/$USERNAME"
+    # fi
 
-    echo "${USERNAME}:${PASSWORD}" | chpasswd
-    chage -d 0 "$USERNAME"
-
-    echo
+	echo "${USERNAME}:${PASSWORD}" | chpasswd
+	chage -d 0 "$USERNAME"
+	    
+	echo
 
     echo "----- Last entry -----"
-
     tail -n 1 /etc/passwd
-
     echo
-
     echo "----- Account Information -----"
-
     echo "username : ${USERNAME}"
-
     echo "password : ${PASSWORD}"
-
     echo "host : $(hostname)"
+
 else
      echo "Please run with sudo as root"
      echo "${?}"
