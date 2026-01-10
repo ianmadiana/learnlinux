9801bd4ad22e22810edced833c008374783ca57c
Ian Madiana <ianmadiana@gmail.com>
2026-01-10 20:00:02 +0700

    fix script 1

--- labs/split-demo.sh
+++ labs/split-demo.sh
@@ -3,26 +3,29 @@
 
 # script for split msisdn
 
+# dir file
 DIR_FILE=/home/mobaxterm/Documents/learn/LABS/source/msisdn/MSISDN.txt
 
 echo "Split file begin"
 
-
+# command for splitting file msisdn
 split -l 5000 --additional-suffix=.txt $DIR_FILE msisdn_
 
-echo "$STATUS"
+STATUS=${?}
 
-echo
-
+# cek if status 0 / success
 if [[ $STATUS -eq 0 ]] 
 then 
+# print success
 echo "split success"
+# else print failed with exit status
 else
 echo "split failed with exit status : ${?}"
 fi
 
+# print result
 echo
-echo "Result split:"
+echo "Result:"
 ls -lrt msisdn_*.txt
 
 
